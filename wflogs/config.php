2cb10b77f5045f669f70d31e49bc4dfdfa6bb4cc
Obaid26 <Obaid26@github.com>
2020-04-19 16:07:51 -0700

    Exclude non-custom folders/files.
    First commit that is copied from master in this branch. Will only consist of:
    themes/book-writer
    plugins/book-writer
    plugins/advanced-favorites
    plugins/personalize-logins
    
    Only files that are actually being contributed to.

--- wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"*YBREH7oGgza)h=N+%.E5YJ{I{VF(~FeqWEBU=3<qf,JmM&!;t?~8+ &$Xd}Tmtw";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:442;}
