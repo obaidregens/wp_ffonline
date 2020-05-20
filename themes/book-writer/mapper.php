b57ea4eb0923f9fbf6a4584fea58c4316df6c52e
fnewlkfm <fkjwqenjkfn@nmgfk4wmr.com>
2020-05-20 12:58:47 -0700

    Version 2.2.4
    Improved sitemap generator to split into files as server could not generate all in one.

--- themes/book-writer/mapper.php
+++ themes/book-writer/mapper.php
@@ -1,6 +1,10 @@
 <?php
 define('WP_USE_THEMES', false);
 require(explode('wp-content',__FILE__)[0] . 'wp-load.php');
+$dir = explode('wp-content',__FILE__)[0] . 'sitemap';
+if (! file_exists($dir)){
+    mkdir($dir);
+}
 
 function f_stamp($time){
     return gmdate(DATE_W3C,$time);
@@ -16,52 +20,100 @@ function url_field($loc,$lastmod,$changefreq){
     $construct .= '</url>';
     return $construct;
 }
+$xml = '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 $book_query = new WP_Query( array(
     'post_type'              => array( 'book' ),
     'post_status'            => array( 'publish' ),
     'order'                  => 'DESC',
     'orderby'                => 'modified',
-    'posts_per_page'		 => -1,
+    'posts_per_page'		 => 10,
 ) );
-$books = $book_query->posts;
-
-$xml = '<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 //Main Page
 $xml .= url_field(
     'https://fanfiction.online/',
-    f_dt($books[0]->post_modified),
+    f_dt($book_query->posts[0]->post_modified),
     'hourly'
 );
 //Pages
-$num_pages = intval($book_query->found_posts/10)+1;
+$num_pages = $book_query->max_num_pages;
 for ($i=2; $i <= $num_pages; $i++) { 
     $xml .= url_field(
         'https://fanfiction.online/?page=' . $i,
-        f_dt($books[0]->post_modified),
+        f_dt($book_query->posts[0]->post_modified),
         'hourly'
     );
 }
-//Books
-foreach($books as $book){
-    $xml .= url_field(
-        get_permalink($book->ID),
-        f_dt($book->post_modified),
-        'monthly'
-    );
-    //Chapters
-    $chapters = published_chapters($book->ID);
-    foreach($chapters as $chapter){
-        $xml .= url_field(
-            get_permalink($chapter->ID),
-            f_dt($chapter->post_modified),
-            'weekly'
-        );
+
+//Collection Page
+$terms = (new WP_Term_Query(array(
+    'count'         => 1,
+    'fields'        => 'ids',
+    'taxonomy' 		=> 'collection',
+    'hide_empty' 	=> true,
+    'meta_query' 	=> array(
+        'relation' 		=> 'AND',
+        array(
+            'key'     	=> 'public_collection',
+            'value'   	=> 'Public',
+            'compare' 	=> '=',
+            'type'    	=> 'CHAR',
+        ),
+    ),
+    'meta_key'      => 'time_modified',
+    'orderby'       => 'meta_value_num',
+    'order'         => 'DESC'
+)))->terms;
+$xml .= url_field(
+    'https://fanfiction.online/collection/',
+    f_stamp(get_term_meta($terms[0],'time_modified',true)),
+    'daily'
+);
+$xml .= '</urlset>';
+file_put_contents ($dir . '/sitemap-general.xml',$xml);
+
+$book_query = new WP_Query( array(
+    'post_type'              => array( 'book' ),
+    'post_status'            => array( 'publish' ),
+    'order'                  => 'DESC',
+    'orderby'                => 'modified',
+    'posts_per_page'		 => 100,
+) );
+$num_pages = $book_query->max_num_pages;
+for ($i=1; $i <= $num_pages; $i++) {
+    $file = $dir . '/sitemap-book-' . $i . '.xml';
+    if (! file_exists($file) || filemtime($file) < time() - 172800){
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';        
+        $books = (new WP_Query(array(
+            'post_type'              => array( 'book' ),
+            'post_status'            => array( 'publish' ),
+            'order'                  => 'DESC',
+            'orderby'                => 'modified',
+            'posts_per_page'		 => 100,
+            'paged'                  => $i,
+        )))->posts;
+        foreach($books as $book){
+            $xml .= url_field(
+                get_permalink($book->ID),
+                f_dt($book->post_modified),
+                'monthly'
+            );
+            //Chapters
+            $chapters = published_chapters($book->ID);
+            foreach($chapters as $chapter){
+                $xml .= url_field(
+                    get_permalink($chapter->ID),
+                    f_dt($chapter->post_modified),
+                    'weekly'
+                );
+            }
+        }
+        $xml .= '</urlset>';
+        file_put_contents ($file,$xml);
     }
 }
-
-$args = array(
-    'taxonomy' 		=> 'collection',
+$num_pages = intval(wp_count_terms('collection',array(
     'hide_empty' 	=> true,
 	'meta_query' 	=> array(
 		'relation' 		=> 'AND',
@@ -75,64 +127,108 @@ $args = array(
     'meta_key'      => 'time_modified',
     'orderby'       => 'meta_value_num',
     'order'         => 'DESC',
-);	
-$terms = (new WP_Term_Query( $args ))->terms;
-//Collection Page
-$xml .= url_field(
-    'https://fanfiction.online/collection/',
-    f_stamp(get_term_meta($terms[0]->term_id,'time_modified',true)),
-    'daily'
-);
-foreach($terms as $term){
-    $xml .= url_field(
-        get_term_link($term->term_id),
-        f_stamp(get_term_meta($term->term_id,'time_modified',true)),
-        'weekly'
-    );
-}
-//Users
-$users = get_users( array() );
-foreach($users as $user_obj){
-    $user = $user_obj->data;
-    $author_books = (new WP_Query( array(
-        'author__in'             => array($user->ID),
-        'post_type'              => array( 'book' ),
-        'post_status'            => array( 'publish' ),
-        'order'                  => 'DESC',
-        'orderby'                => 'modified',
-        'posts_per_page'		 => -1,
-    ) ))->posts;
-    if (empty($author_books)){
-        $author_time = f_dt($user->user_registered);
-    }
-    else{
-        $author_time = f_dt($author_books[0]->post_modified);
-    }
-    $xml .= url_field(
-        get_author_posts_url($user->ID),
-        $author_time,
-        'monthly'
-    );
-    $xml .= url_field(
-        str_replace('//','/',get_author_posts_url($user->ID) . '/books'),
-        $author_time,
-        'monthly'
-    );
-    $xml .= url_field(
-        str_replace('//','/',get_author_posts_url($user->ID) . '/updates'),
-        $author_time,
-        'monthly'
-    );
-    $xml .= url_field(
-        str_replace('//','/',get_author_posts_url($user->ID) . '/collection'),
-        $author_time,
-        'monthly'
-    );
-}
-$xml .= '</urlset>';
+))/100)+1;
+for ($i=1; $i <= $num_pages; $i++) {
+    $file = $dir . '/sitemap-collection-' . $i . '.xml';
+    if (! file_exists($file) || filemtime($file) < time() - 172800){
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-$dir = explode('wp-content',__FILE__)[0] . 'sitemap';
-if (! file_exists($dir)){
-    mkdir($dir);
+        $terms = (new WP_Term_Query(array(
+            'count'         => 100,
+            'offset'        => ($i * 100) - 100,
+            'taxonomy' 		=> 'collection',
+            'hide_empty' 	=> true,
+            'meta_query' 	=> array(
+                'relation' 		=> 'AND',
+                array(
+                    'key'     	=> 'public_collection',
+                    'value'   	=> 'Public',
+                    'compare' 	=> '=',
+                    'type'    	=> 'CHAR',
+                ),
+            ),
+            'meta_key'      => 'time_modified',
+            'orderby'       => 'meta_value_num',
+            'order'         => 'DESC'
+        )))->terms;
+
+        foreach($terms as $term){
+            $xml .= url_field(
+                get_term_link($term->term_id),
+                f_stamp(get_term_meta($term->term_id,'time_modified',true)),
+                'weekly'
+            );
+        }
+        $xml .= '</urlset>';
+        file_put_contents ($file,$xml);
+    }
 }
-file_put_contents ($dir . '/sitemap.xml',$xml);
+
+//Users
+$users = new WP_User_Query( array(
+    'number'    => 1,
+    'paged'     => 1
+) );
+$num_pages = intval($users->get_total()/100)+1;
+for ($i=1; $i <= $num_pages; $i++) {
+    $file = $dir . '/sitemap-author-' . $i . '.xml';
+    if (! file_exists($file) || filemtime($file) < time() - 172800){
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        $users = (new WP_User_Query( array(
+            'number'    => 100,
+            'paged'     => $i
+        ) ))->get_results();
+        foreach($users as $user_obj){
+            $user = $user_obj->data;
+            $author_books = (new WP_Query( array(
+                'author__in'             => array($user->ID),
+                'post_type'              => array( 'book' ),
+                'post_status'            => array( 'publish' ),
+                'order'                  => 'DESC',
+                'orderby'                => 'modified',
+                'posts_per_page'		 => 1,
+            ) ))->posts;
+            if (empty($author_books)){
+                $author_time = f_dt($user->user_registered);
+            }
+            else{
+                $author_time = f_dt($author_books[0]->post_modified);
+            }
+            $xml .= url_field(
+                get_author_posts_url($user->ID),
+                $author_time,
+                'monthly'
+            );
+            $xml .= url_field(
+                str_replace('//','/',get_author_posts_url($user->ID) . '/books'),
+                $author_time,
+                'monthly'
+            );
+            $xml .= url_field(
+                str_replace('//','/',get_author_posts_url($user->ID) . '/updates'),
+                $author_time,
+                'monthly'
+            );
+            $xml .= url_field(
+                str_replace('//','/',get_author_posts_url($user->ID) . '/collection'),
+                $author_time,
+                'monthly'
+            );
+        }
+        $xml .= '</urlset>';
+        file_put_contents ($file,$xml);
+    }
+}
+
+//Sitemap Index
+$index_xml = '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+$all_files = scandir($dir);
+foreach ($all_files as $file_) {
+    if (strpos($file_,'sitemap') !== false){
+        $index_xml .= '<sitemap><loc>https://fanfiction.online/sitemap/' . $file_ . '</loc></sitemap>';
+    }
+}
+$index_xml .= '</sitemapindex>';
+file_put_contents ($dir . '/sitemap-index.xml',$index_xml);
