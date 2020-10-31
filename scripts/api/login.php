28360c4f28f6eb645d7f294ff100cc5a71bdd66a
fnewlkfm <fkjwqenjkfn@nmgfk4wmr.com>
2020-10-31 01:59:36 -0700

    Version 15.5.5 - Fix

--- scripts/api/login.php
+++ scripts/api/login.php
@@ -38,7 +38,6 @@ function api_username_check () {
         'username'  => $_POST['data']['username'],
     ],['username']))->return;
     if (err::is($validation)){
-        $i = array_key_first($validation->errors);
         return [
             'code'      => 7,
             'error'    => $validation->errors[0]['error']
