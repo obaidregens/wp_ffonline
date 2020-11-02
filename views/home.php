0f3a431364a663733e274139bda86fde840fee99
fnewlkfm <fkjwqenjkfn@nmgfk4wmr.com>
2020-11-02 15:54:59 -0800

    Version 15.5.12
    Revert

--- views/home.php
+++ views/home.php
@@ -13,9 +13,8 @@ $app->bundle->enqueue();
     <section-description>
     Welcome to Fanfiction Online
     </section-description>
-    <mini-description class="action">
-    <a theme class="button" href="/read">Start Reading</a>
-    <a theme class="button" href="/write">Start Writing</a>
+    <mini-description>
+    Scroll down to see why you've come to the right place
     </mini-description>
 </section>
 <!-- Write -->
